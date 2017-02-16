9583001e5d0f1f5ade375025b06b8c90f09f6cd9
LouiseBC <louisehmbc@gmail.com>
2017-02-16 11:40:44 -0500

    Add files via upload

---
+++ 6.scm
@@ -0,0 +1,26 @@
+;;Euler 6: Sum square difference
+
+(define (square x)
+		(* x x))
+
+(define (n-sum-squared sum counter max)
+		(if (> counter max)
+			sum
+			(n-sum-squared (+ sum (square counter) )
+						   (+ counter 1)
+						   max
+			)
+		)
+)
+
+(define (sum-of-n sum counter max)
+      (if (> counter max)
+          sum
+          (sum-of-n (+ sum counter)
+          			(+ counter 1)
+         			max
+          )
+      )
+)
+
+(display (- (square(sum-of-n 0 1 100)) (n-sum-squared 0 1 100)))
