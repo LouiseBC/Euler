b4421ca9a7a52f27ec69a5e95cfd89b41faac457
LouiseBC <louisehmbc@gmail.com>
2017-02-20 09:17:07 -0500

    Update 6.scm

--- 6.scm
+++ 6.scm
@@ -6,15 +6,15 @@
 (define (n-sum-squared sum counter max)
 		(if (> counter max)
 			sum
-			(n-sum-squared (+ sum (square counter) )
-						   (+ counter 1)
-						   max)))
+			(n-sum-squared  (+ sum (square counter) )
+					(+ counter 1)
+					max)))
 
 (define (sum-of-n sum counter max)
       (if (> counter max)
           sum
           (sum-of-n (+ sum counter)
-          			(+ counter 1)
-         			max)))
+          	    (+ counter 1)
+         	     max)))
 
 (display (- (square(sum-of-n 0 1 100)) (n-sum-squared 0 1 100)))
